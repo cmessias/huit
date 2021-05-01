7873a11a904caa61f075cc8b8699865c9c7e4e63
Caio Messias <caio.messias@pm.me>
2021-05-01 18:33:28 -0300

    Cast x and y as usize

--- src/cpu.rs
+++ src/cpu.rs
@@ -36,14 +36,6 @@ fn to_nn(opcode: u16) -> u8 {
     return (opcode & 0x00FF) as u8;
 }
 
-fn to_x(x: u8) -> usize {
-    return x as usize;
-}
-
-fn to_y(y: u8) -> usize {
-    return y as usize;
-}
-
 pub struct Cpu {
     pub memory: [u8; MEMORY_SIZE],
     pub display: [Pixel; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -122,8 +114,8 @@ impl Cpu {
 
     fn decode(&self, opcode: u16) -> Instruction {
         let a = ((opcode & 0xF000) >> 12) as u8;
-        let b = ((opcode & 0x0F00) >> 8) as u8;
-        let c = ((opcode & 0x00F0) >> 4) as u8;
+        let b = ((opcode & 0x0F00) >> 8) as usize;
+        let c = ((opcode & 0x00F0) >> 4) as usize;
         let d = (opcode & 0x000F) as u8;
 
         return match (a, b, c, d) {
@@ -131,40 +123,40 @@ impl Cpu {
             (0, 0, 0xE, 0xE) => Box::new(|cpu| ret(cpu)),
             (1, _, _, _) => Box::new(move |cpu| jmp(cpu, to_nnn(opcode))),
             (2, _, _, _) => Box::new(move |cpu| call(cpu, to_nnn(opcode))),
-            (3, x, _, _) => Box::new(move |cpu| skipx_eq(cpu, to_x(x), to_nn(opcode))),
-            (4, x, _, _) => Box::new(move |cpu| skipx_neq(cpu, to_x(x), to_nn(opcode))),
-            (5, x, y, 0) => Box::new(move |cpu| skipxy_eq(cpu, to_x(x), to_y(y))),
-            (6, x, _, _) => Box::new(move |cpu| setx(cpu, to_x(x), to_nn(opcode))),
-            (7, x, _, _) => Box::new(move |cpu| addx(cpu, to_x(x), to_nn(opcode))),
-            (8, x, y, 0) => Box::new(move |cpu| setxy(cpu, to_x(x), to_y(y))),
-            (8, x, y, 1) => Box::new(move |cpu| orxy(cpu, to_x(x), to_y(y))),
-            (8, x, y, 2) => Box::new(move |cpu| andxy(cpu, to_x(x), to_y(y))),
-            (8, x, y, 3) => Box::new(move |cpu| xorxy(cpu, to_x(x), to_y(y))),
-            (8, x, y, 4) => Box::new(move |cpu| addxyc(cpu, to_x(x), to_y(y))),
-            (8, x, y, 5) => Box::new(move |cpu| subxy(cpu, to_x(x), to_y(y))),
-            (8, x, y, 6) => Box::new(move |cpu| shiftxr(cpu, to_x(x))),
+            (3, x, _, _) => Box::new(move |cpu| skipx_eq(cpu, x, to_nn(opcode))),
+            (4, x, _, _) => Box::new(move |cpu| skipx_neq(cpu, x, to_nn(opcode))),
+            (5, x, y, 0) => Box::new(move |cpu| skipxy_eq(cpu, x, y)),
+            (6, x, _, _) => Box::new(move |cpu| setx(cpu, x, to_nn(opcode))),
+            (7, x, _, _) => Box::new(move |cpu| addx(cpu, x, to_nn(opcode))),
+            (8, x, y, 0) => Box::new(move |cpu| setxy(cpu, x, y)),
+            (8, x, y, 1) => Box::new(move |cpu| orxy(cpu, x, y)),
+            (8, x, y, 2) => Box::new(move |cpu| andxy(cpu, x, y)),
+            (8, x, y, 3) => Box::new(move |cpu| xorxy(cpu, x, y)),
+            (8, x, y, 4) => Box::new(move |cpu| addxyc(cpu, x, y)),
+            (8, x, y, 5) => Box::new(move |cpu| subxy(cpu, x, y)),
+            (8, x, y, 6) => Box::new(move |cpu| shiftxr(cpu, x)),
             //(8, x, y, 6) => Box::new(move |cpu| shiftxyr(cpu, to_x(x), to_y(y))),
-            (8, x, y, 7) => Box::new(move |cpu| subyx(cpu, to_x(x), to_y(y))),
-            (8, x, y, 0xE) => Box::new(move |cpu| shiftxl(cpu, to_x(x))),
+            (8, x, y, 7) => Box::new(move |cpu| subyx(cpu, x, y)),
+            (8, x, y, 0xE) => Box::new(move |cpu| shiftxl(cpu, x)),
             //(8, x, y, 0xE) => Box::new(move |cpu| shiftxyl(cpu, to_x(x), to_y(y))),
-            (9, x, y, 0) => Box::new(move |cpu| skipxy_neq(cpu, to_x(x), to_y(y))),
+            (9, x, y, 0) => Box::new(move |cpu| skipxy_neq(cpu, x, y)),
             (0xA, _, _, _) => Box::new(move |cpu| seti(cpu, to_nnn(opcode))),
             (0xB, _, _, _) => Box::new(move |cpu| jmp0(cpu, to_nnn(opcode))),
-            (0xC, x, _, _) => Box::new(move |cpu| rand(cpu, to_x(x), to_nn(opcode))),
-            (0xD, x, y, n) => Box::new(move |cpu| draw(cpu, x as usize, y as usize, n as usize)),
-            (0xE, x, 9, 0xE) => Box::new(move |cpu| skipk(cpu, x as usize)),
-            (0xE, x, 0xA, 1) => Box::new(move |cpu| skipnk(cpu, x as usize)),
-            (0xF, x, 0, 0xA) => Box::new(move |cpu| get_key(cpu, x as usize)),
-            (0xF, x, 1, 0xE) => Box::new(move |cpu| addi(cpu, to_x(x))),
-            (0xF, x, 2, 9) => Box::new(move |cpu| font_char(cpu, to_x(x))),
-            (0xF, x, 3, 3) => Box::new(move |cpu| bcd(cpu, to_x(x))),
-            (0xF, x, 5, 5) => Box::new(move |cpu| stx(cpu, to_x(x))),
+            (0xC, x, _, _) => Box::new(move |cpu| rand(cpu, x, to_nn(opcode))),
+            (0xD, x, y, n) => Box::new(move |cpu| draw(cpu, x, y, n as usize)),
+            (0xE, x, 9, 0xE) => Box::new(move |cpu| skipk(cpu, x)),
+            (0xE, x, 0xA, 1) => Box::new(move |cpu| skipnk(cpu, x)),
+            (0xF, x, 0, 0xA) => Box::new(move |cpu| get_key(cpu, x)),
+            (0xF, x, 1, 0xE) => Box::new(move |cpu| addi(cpu, x)),
+            (0xF, x, 2, 9) => Box::new(move |cpu| font_char(cpu, x)),
+            (0xF, x, 3, 3) => Box::new(move |cpu| bcd(cpu, x)),
+            (0xF, x, 5, 5) => Box::new(move |cpu| stx(cpu, x)),
             //(0xF, x, 5, 5) => Box::new(move |cpu| stxi(cpu, to_x(x))),
-            (0xF, x, 6, 5) => Box::new(move |cpu| ldx(cpu, to_x(x))),
+            (0xF, x, 6, 5) => Box::new(move |cpu| ldx(cpu, x)),
             //(0xF, x, 6, 5) => Box::new(move |cpu| ldxi(cpu, to_x(x))),
-            (0xF, x, 0, 7) => Box::new(move |cpu| lddt(cpu, to_x(x))),
-            (0xF, x, 1, 5) => Box::new(move |cpu| stdt(cpu, to_x(x))),
-            (0xF, x, 1, 8) => Box::new(move |cpu| stst(cpu, to_x(x))),
+            (0xF, x, 0, 7) => Box::new(move |cpu| lddt(cpu, x)),
+            (0xF, x, 1, 5) => Box::new(move |cpu| stdt(cpu, x)),
+            (0xF, x, 1, 8) => Box::new(move |cpu| stst(cpu, x)),
             _ => panic!("unknown instruction: {}", opcode),
         };
     }
